b6216cc9bc5dbd4a6b77a45ac542f7f8bdbbf265
Sennio <b.bosse1991@gmail.com>
2024-10-06 21:16:42 +0200

    Fixes

--- game/ld56_godot/scripts/flying_enemy_controller.gd
+++ game/ld56_godot/scripts/flying_enemy_controller.gd
@@ -3,12 +3,19 @@ class_name FlyingEnemyController extends RigidBody3D
 @export var target: Node3D
 
 const SPEED = 1
+const JITTER_INTENSITY = 8.0
 
 func _physics_process(delta: float) -> void:
-	# Calculate the direction towards the target
 	var direction: Vector3 = Vector3.ZERO
-	
+
 	if target:
 		direction = (target.global_position - global_position).normalized()
-	
+
+		var jitter = Vector3(
+			0.0,
+			randf_range(-JITTER_INTENSITY, JITTER_INTENSITY),
+			randf_range(-JITTER_INTENSITY, JITTER_INTENSITY)
+		)
+		direction += jitter
+
 		apply_central_force(direction * SPEED)
