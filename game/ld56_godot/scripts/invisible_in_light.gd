c1c90586fe62d8084f1ee6d6e3b670f92b467241
Sennio <b.bosse1991@gmail.com>
2024-10-09 13:51:50 +0200

    Fixed firefly glows

--- game/ld56_godot/scripts/invisible_in_light.gd
+++ game/ld56_godot/scripts/invisible_in_light.gd
@@ -2,12 +2,18 @@ class_name InvisibleInLight extends Node3D
 
 @export var light_trigger_area: Area3D
 
+var num_entered_areas = 0
+
 func _ready() -> void:
 	light_trigger_area.area_entered.connect(on_area_entered)
 	light_trigger_area.area_exited.connect(on_area_exited)
 
 func on_area_entered(area: Area3D) -> void:
 	visible = false
+	num_entered_areas += 1
 
 func on_area_exited(area: Area3D) -> void:
-	visible = true
+	num_entered_areas -= 1
+	if num_entered_areas <= 0:
+		num_entered_areas = 0
+		visible = true
